b69610f96cf4de10309932c78d4755646853a7d8
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-10-13 01:03:43 -0300

    fix (Home view): Change aspect-radio for padding strategy

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -12,15 +12,19 @@ export const Home = ({ onPicked }: HomeI) => {
 
   return (
     <main className="Home">
-      <button type="button" onClick={handleSelected("paper")}>
-        <Option option="paper" />
-      </button>
-      <button type="button" onClick={handleSelected("scissors")}>
-        <Option option="scissors" />
-      </button>
-      <button type="button" onClick={handleSelected("rock")}>
-        <Option option="rock" />
-      </button>
+      <div className="Home__container">
+        <div className="Home__content">
+          <button type="button" onClick={handleSelected("paper")}>
+            <Option option="paper" />
+          </button>
+          <button type="button" onClick={handleSelected("scissors")}>
+            <Option option="scissors" />
+          </button>
+          <button type="button" onClick={handleSelected("rock")}>
+            <Option option="rock" />
+          </button>
+        </div>
+      </div>
     </main>
   );
 };
